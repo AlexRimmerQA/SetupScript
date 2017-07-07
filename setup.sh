99e714217efc68b32d1f6f9c4c49a83d2b1de212
Alex Rimmer <alex.rimmer@qa.com>
2017-07-07 08:41:57 +0000

    Docker and jenkins install

--- setup.sh
+++ setup.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
+
+#Basic update
 sudo apt-get update
+
+#Install Git
 sudo apt-get install -qq -y git
+
+#Install Docker
 sudo wget -pO- https://get.docker.com/ | sh
 sudo service docker restart
+sudo usermod -aG docker ubuntu
 
+#Install Jenkins
+sudo docker pull jenkins
+sudo docker run -d --name jenkins_instance -p 8080:8080 jenkins
